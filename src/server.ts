9079682a165cb0344e854436957333216e14f180
Aayush Agnihotri <aa2328@cornell.edu>
2025-09-25 22:31:12 -0400

    Changing port

--- src/server.ts
+++ src/server.ts
@@ -26,9 +26,7 @@ router.use('/teams', teamRouter);
 router.use('/projects', projectRouter);
 router.use('/containers', containerRouter);
 
-app.use('/api', router);
-
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '8000';
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
